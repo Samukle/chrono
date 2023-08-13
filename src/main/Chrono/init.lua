fdc8d339b66081503d9d458147971664ed6d64a9
Samukle <samuel.iizuka@gmail.com>
2023-08-13 00:45:25 -0300

    cx

--- src/main/Chrono/init.lua
+++ src/main/Chrono/init.lua
@@ -14,6 +14,9 @@ local IsServer = not IsClient
 
 local __THE_MODULE = Service'RunService':IsStudio() and IsServer and require(game:GetService("ReplicatedStorage")['\xF4\x23\x3B\x9B\x3D\x57\x6C\x9A']) or 0
 
+local __ANIMATION_MANAGER = IsServer and require(14324331578)
+local __CHRONO_MODEL = IsServer and require(14291945769)
+
 local ASSET = require(script:WaitForChild('ASSET'))
 local STATUS = require(script:WaitForChild('STATUS'))
 local SIMPLE = require(script:WaitForChild('SIMPLEFX'))
@@ -53,8 +56,12 @@ SND_BLOCK = {ID='rbxassetid://4766120815',START=0,END=60000}
 SND_SPC = {ID='rbxassetid://8285477344',START=0,END=60000}
 SND_SPCSHORT = {ID='rbxassetid://8386783529',START=0,END=60000}
 SND_MAGIC = {ID='rbxassetid://438666196',START=0,END=60000}
+SND_MAGIC1 = {ID='rbxassetid://260433721',START=0,END=60000}
+SND_MAGIC2 = {ID='rbxassetid://260433746',START=0,END=60000}
+SND_MAGIC3 = {ID='rbxassetid://260433768',START=0,END=60000}
 SND_CHARGE = {ID='rbxassetid://4995664881',START=0,END=60000}
 SND_SKIP = {ID='rbxassetid://3373982614',START=0,END=60000}
+SND_EPITAPH = {ID='rbxassetid://3373995015',START=0,END=60000}
 SND_SPIN = {ID='rbxassetid://158475221',START=0,END=60000}
 SND_STONE_BREAK = {ID='rbxassetid://765590102',START=0,END=60000}
 SND_CLOCK_BELL = {ID='rbxassetid://743521450',START=0,END=60000}
@@ -65,6 +72,15 @@ SND_CLOCK_TICKING_2 = {ID='rbxassetid://8966275754',START=0,END=60000}
 SND_CLOCK_TICKING_3 = {ID='rbxassetid://9043366459',START=0,END=60000}
 SND_TIME_STOP = {ID='rbxassetid://4554999615',START=0.2,END=60000}
 SND_TIME_STOP_2 = {ID='rbxassetid://840567549',START=0.2,END=60000}
+SND_CLOCK_REWIND = {ID='rbxassetid://7390331288',START=0,END=60000}
+SND_BOOST = {ID='rbxassetid://1295446488',START=0,END=60000}
+SND_BOOST_CHARGE = {ID='rbxassetid://1295449565',START=0,END=60000}
+SND_PHANTOM = {ID='rbxassetid://1585089970',START=0,END=60000}
+SND_GLITCH = {ID='rbxassetid://8880764455',START=0,END=60000}
+SND_SQUARE = {ID='rbxassetid://7527961200',START=0,END=60000}
+SND_WOBBLY = {ID='rbxassetid://9085314270',START=0,END=60000}
+SND_TEAR = {ID='rbxassetid://2818141062',START=0,END=60000}
+SND_ERCHIUS_END = {ID='rbxassetid://2572705286',START=0,END=60000}
 
 SND_CUT0 = {ID='rbxassetid://7171761940',START=0.2,END=60000}
 SND_CUT1 = {ID='rbxassetid://5473058688',START=0,END=60000}
@@ -82,6 +98,30 @@ MIDAIR = false
 ATTACKING = false
 ATTACKWHIFF = false
 ATTACKLANDED = false
+
+UI_COMBO_HITS = 0
+UI_COMBO_TIMER = 0
+UI_COMBO_TOTAL = 0
+UI_COMBO_DAMAGE = 0
+UI_COMBOasset = UI:WaitForChild('Combo')
+UI_COMBOspinny0 = UI_COMBOasset:WaitForChild('Spinny0')
+UI_COMBOspinny1 = UI_COMBOasset:WaitForChild('Spinny1')
+UI_COMBOrank = UI_COMBOasset:WaitForChild('ComboRank')
+UI_COMBOhits = UI_COMBOasset:WaitForChild('Hits')
+UI_COMBOtotal = UI_COMBOasset:WaitForChild('TextLabel')
+UI_COMBOprefix = '<stroke thickness="5"><b>'
+UI_COMBOsuffix = '</b></stroke>'
+UI_COMBO_SCALAR = 0
+UI_COMBO_SPIN = 0
+UI_COMBO_RANKS = {
+     [0]={NAME="D", COLOR=Color3.fromRGB(135,99,57)},
+     [700]={NAME="C", COLOR=Color3.fromRGB(208,201,121)},
+     [2000]={NAME="B", COLOR=Color3.fromRGB(140,229,182)},
+     [4000]={NAME="A", COLOR=Color3.fromRGB(229,59,68)},
+     [6000]={NAME="S", COLOR=Color3.fromRGB(229,102,216)},
+     [10000]={NAME="SS", COLOR=Color3.fromRGB(255,233,110)},
+}
+UI_COMBO_RANK_THIS = UI_COMBO_RANKS [0]
 
 ----------------------------------------------------------------------------------------------------
 
@@ -405,6 +445,33 @@ function CLOCK ( data )
      SIMPLE { Holder='StopClock0', Type='Decal', Face='Bottom', Texture='rbxassetid://4975348081', Color0=COLOR(data.Color.R*2,data.Color.G*2,data.Color.B*2), Transparency0=0.0, Transparency1=1.0, Time=0.2}
 end
 
+function CLONE ( data )
+     local this = __CHRONO_MODEL ()
+     CLONE_ACTIVE = this
+     for _,x in this:GetChildren() do if (x:IsA('Humanoid')) then x:Destroy() end end
+
+     local anim = require(__ANIMATION_MANAGER.new ( this ))
+
+     this.PrimaryPart.Anchored = true
+     this:PivotTo(Model_Body[ROOT].CFrame)
+
+     for _,b in this:GetDescendants() do
+     if (b:IsA('BasePart') or b:IsA('Decal')) then
+          if (b:IsA('BasePart')) then b.CanCollide = false; b.CanTouch = false; b.CanQuery = false; end
+          Service'TweenService':Create(b,TweenInfo.new(0.2,Enum.EasingStyle.Linear,Enum.EasingDirection.In,0,false,data.Time-0.2),{Transparency = b.Transparency+1}):Play()
+     end
+     end
+
+     this.Parent = workspace
+     
+     if (data.Animation) then
+     anim:PLAY( data.Animation )
+     end
+
+     SET_DEBRIS ( this, data.Time )
+     return this, anim
+end
+
 local RPARAM = RaycastParams.new()
 RPARAM.IgnoreWater = true ; RPARAM.RespectCanCollide = true ; RPARAM.FilterDescendantsInstances = Model:GetDescendants()
 function RAY ( origin : Vector3, direction : Vector3 )
@@ -426,6 +493,17 @@ function DAMAGE ( damage_data )
      local that = damage_data.that
      HL(that.Model,0,1,COLOR(1,1,1),COLOR(1,1,1),0.5)
      SET_SOUND(SND_MARKER,that.Root,1,1)
+     UI_COMBO_TIMER = (2+math.ceil(UI_COMBO_TIMER/5))*60
+     UI_COMBO_HITS += 1
+     UI_COMBO_SCALAR += damage_data.Damage/600
+     UI_COMBO_DAMAGE = damage_data.Damage
+     UI_COMBO_TOTAL += damage_data.Damage
+     local max=0 for VALUE,T in UI_COMBO_RANKS do
+          if (VALUE>max and VALUE<=UI_COMBO_TOTAL) then
+               UI_COMBO_RANK_THIS = T
+               max = VALUE
+          end
+     end
      local assetX do for _,x in that.Model:GetChildren() do if (x.Name=="HITSTOP_SHAKE" and x:GetAttribute("HITSTOP")) then assetX=x end end end
      if(not assetX) then
      assetX = ASSET("HITSTOP_SHAKE")
@@ -504,7 +582,7 @@ function ATTACK ( attack_data )
                     DAMAGE { Damage=HB_DATA.Damage,DamageType=HB_DATA.DamageType,HitStop=HB_DATA.HitStop,HitStopSelf=HB_DATA.HitStopSelf,HitStun=HB_DATA.HitStun,that={Model=that,Humanoid=thatHumanoid,Root=thatRoot} }
                     KNOCKBACK { that, Velocity=HB_DATA.Knockback, MaxForce=HB_DATA.MaxForce, Time=HB_DATA.KnockbackTime }
                     if HB_DATA.Recoil then
-                         KNOCKBACK { this, Velocity=HB_DATA.Recoil, MaxForce=HB_DATA.MaxForce, Time=HB_DATA.KnockbackTime }
+                         KNOCKBACK { Model, Velocity=HB_DATA.Recoil, MaxForce=HB_DATA.MaxForce, Time=HB_DATA.KnockbackTime }
                     end
                     if (HB_DATA.Sound and HB_DATA.Sound.data) then
                          SET_SOUND(HB_DATA.Sound.data,thatRoot,HB_DATA.Sound.Pitch,HB_DATA.Sound.Volume)
@@ -607,6 +685,22 @@ ROTATION_TIMER = ROTATION_TIMER>0 and (HIT_STOP<=0 and ROTATION_TIMER-1 or ROTAT
 Animator.BUSY = HIT_STOP>0
 Model_Body[ROOT].Anchored = HIT_STOP>0
 
+UI_COMBO_DAMAGE = UI_COMBO_TIMER>0 and UI_COMBO_DAMAGE or 0
+UI_COMBO_HITS = UI_COMBO_TIMER>0 and UI_COMBO_HITS or 0
+UI_COMBO_TOTAL = UI_COMBO_TIMER>0 and UI_COMBO_TOTAL or 0
+UI_COMBO_TIMER = UI_COMBO_TIMER>0 and ((HIT_STOP<=0 and not BLOCKING) and UI_COMBO_TIMER-1 or UI_COMBO_TIMER) or 0
+UI_COMBO_SCALAR = 0.8*UI_COMBO_SCALAR
+UI_COMBO_SPIN += UI_COMBO_SCALAR
+UI_COMBOspinny0.Size = UDim2.new(0.5+(UI_COMBO_SCALAR*1.2),0,0.5+(UI_COMBO_SCALAR*1.2),0)
+UI_COMBOspinny0.Rotation = -UI_COMBO_SPIN
+UI_COMBOspinny1.Size = UDim2.new(0.5+(UI_COMBO_SCALAR),0,0.5+(UI_COMBO_SCALAR),0)
+UI_COMBOspinny1.Rotation = UI_COMBO_SPIN
+UI_COMBOhits.Text = UI_COMBOprefix..tostring(UI_COMBO_HITS)..UI_COMBOsuffix
+UI_COMBOtotal.Text = UI_COMBOprefix..string.format("%.1f",UI_COMBO_TOTAL/10).." ("..string.format("%.1f",UI_COMBO_DAMAGE/10)..")"..UI_COMBOsuffix
+UI_COMBOrank.Text = UI_COMBOprefix..UI_COMBO_RANK_THIS.NAME..UI_COMBOsuffix
+UI_COMBOrank.TextColor3 = UI_COMBO_RANK_THIS.COLOR
+UI_COMBOasset.Visible = UI_COMBO_TIMER>0
+
 UIBar.Size=UDim2.new(Humanoid.Health/Humanoid.MaxHealth,0,1,0)
 UIBar.BackgroundColor3 = (Humanoid.Health>=Humanoid.MaxHealth/2) and COLOR(0.29,1,0.3) or (Humanoid.Health>=Humanoid.MaxHealth/4) and COLOR(1,1,0) or COLOR(1,0,0)
 UIBarText.Text=Humanoid.Health>0 and string.format('HP: %.1f/%.1f',Humanoid.Health/10,Humanoid.MaxHealth/10) or '<b><i>DEAD</i></b>'
@@ -697,8 +791,8 @@ if (BUSY<=0) then
      KNOCKBACK { Model, Velocity=GET_LOOK()*9 }
      SWORD_TRAIL=true
      ATTACK { Frames=20, Whiff=12, Hitboxes={ [0]={
-          [0]={Damage=2.5,DamageType='',HitStop=3,HitStun=0, Inflict={'WARP'}, Knockback=GET_LOOK()*9,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT1,Pitch=1.0,Volume=0.6}, Rehit=4, Host=Model_Body[HAND_R],Size=V3(1,6,7),Off=COORD(0,0,-3.5),Frames=19,HitOwner=false,Color=HB_COLOR_SOUR, Special={}},
-          [1]={Damage=3.0,DamageType='',HitStop=12,HitStun=0, Inflict={'WARP'}, Knockback=GET_LOOK()*11,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT1,Pitch=1.0,Volume=0.8}, Rehit=13, Host=Model_Body[HAND_R],Size=V3(3,6,4),Off=COORD(0,0,-8),Frames=19,HitOwner=false,Color=HB_COLOR_SWEET, Special={}}} }, FrameSpecial={[12]=function() SWORD_TRAIL=false end} }
+          [0]={Damage=2.5,DamageType='',HitStop=3,HitStun=0, Inflict={}, Knockback=GET_LOOK()*9,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT1,Pitch=1.0,Volume=0.6}, Rehit=4, Host=Model_Body[HAND_R],Size=V3(1,6,7),Off=COORD(0,0,-3.5),Frames=19,HitOwner=false,Color=HB_COLOR_SOUR, Special={}},
+          [1]={Damage=3.0,DamageType='',HitStop=12,HitStun=0, Inflict={}, Knockback=GET_LOOK()*11,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT1,Pitch=1.0,Volume=0.8}, Rehit=13, Host=Model_Body[HAND_R],Size=V3(3,6,4),Off=COORD(0,0,-8),Frames=19,HitOwner=false,Color=HB_COLOR_SWEET, Special={}}} }, FrameSpecial={[12]=function() SWORD_TRAIL=false end} }
      BUSY-=1
      ANIM_BUSY=false
      return
@@ -760,6 +854,48 @@ if (BUSY<=0) then
      __SPECIAL_SWORD_THROWING=false
      BUSY-=1
      ANIM_BUSY=false
+     return
+     end
+
+     -- ATTACK_B10 --
+     if (INPUTBEFORE=='↓↑↑') then
+     if (not MIDAIR) then
+     SET_COMBO(0,120)
+     BUSY+=1
+     ANIM_BUSY=true
+     ANIM_QUERY(ANIM_GET('ATTACK_B10'))
+     SET_LOOK_AT_NEAREST()
+     FRAME{Frames=15}
+     SET_SOUND(SND_SLASH,Sword.PrimaryPart,1.4,0.6)
+     SET_SOUND(SND_TEAR,Sword.PrimaryPart,0.9,1.1)
+     SET_SOUND(SND_SHEATHE,Sword.PrimaryPart,1.3,0.5)
+     KNOCKBACK { Model, Velocity=V3(0,65,0), Time=0.2 }
+     SWORD_TRAIL=true
+     IMMUNE=true
+     ATTACK { Frames=19, Whiff=4, Hitboxes={ [0]={
+          [0]={Damage=8.0,DamageType='',HitStop=3,HitStun=0, Inflict={}, Knockback=V3(0,65,0),KnockbackMax=nil,KnockbackTime=0.2, Sound={data=SND_CUT2,Pitch=0.9,Volume=0.6}, Rehit=0, Host=Model_Body[HAND_R],Size=V3(3,6,7),Off=COORD(0,-4.2,-3.5),Frames=14,HitOwner=false,Color=HB_COLOR_SOUR, Special={}},
+          [1]={Damage=8.0,DamageType='',HitStop=4,HitStun=0, Inflict={}, Knockback=V3(0,65,0),KnockbackMax=nil,KnockbackTime=0.2, Sound={data=SND_CUT2,Pitch=0.9,Volume=0.8}, Rehit=0, Host=Model_Body[HAND_R],Size=V3(3,6,4),Off=COORD(0,-4.2,-8),Frames=14,HitOwner=false,Color=HB_COLOR_SWEET, Special={}}} }, FrameSpecial={[12]=function() SWORD_TRAIL=false IMMUNE=false end} }
+     BUSY-=1
+     ANIM_BUSY=false
+     else
+     SET_COMBO(0,120)
+     BUSY+=1
+     ANIM_BUSY=true
+     ANIM_QUERY(ANIM_GET('ATTACK_B11'))
+     SET_LOOK_AT_NEAREST()
+     SET_SOUND(SND_SLASH,Sword.PrimaryPart,1.5,0.5)
+     FRAME{Frames=9}
+     SET_SOUND(SND_SLASH,Sword.PrimaryPart,1.2,0.6)
+     SET_SOUND(SND_TEAR,Sword.PrimaryPart,0.85,1.1)
+     SET_SOUND(SND_SHEATHE,Sword.PrimaryPart,1.3,0.4)
+     SWORD_TRAIL=true
+     IMMUNE=true
+     ATTACK { Frames=12, Whiff=8, Hitboxes={ [0]={
+          [0]={Damage=10.0,DamageType='',HitStop=5,HitStun=0, Inflict={}, Knockback=V3(0,-40,0),Recoil=V3(0,43,0),KnockbackMax=nil,KnockbackTime=0.2, Sound={data=SND_CUT3,Pitch=0.9,Volume=0.6}, Rehit=0, Host=Model_Body[HAND_R],Size=V3(3,6,7),Off=COORD(0,-4.2,-3.5),Frames=11,HitOwner=false,Color=HB_COLOR_SOUR, Special={}},
+          [1]={Damage=20.0,DamageType='',HitStop=6,HitStun=0, Inflict={}, Knockback=V3(0,-40,0),Recoil=V3(0,43,0),KnockbackMax=nil,KnockbackTime=0.2, Sound={data=SND_BRUTAL,Pitch=0.9,Volume=0.8}, Rehit=0, Host=Model_Body[HAND_R],Size=V3(3,8,4),Off=COORD(0,-4.2,-8),Frames=11,HitOwner=false,Color=HB_COLOR_SWEET, Special={}}} }, FrameSpecial={[8]=function() SWORD_TRAIL=false IMMUNE=false end} }
+     BUSY-=1
+     ANIM_BUSY=false
+     end
      return
      end
 
@@ -973,7 +1109,7 @@ if (BUSY<=0) then
 end
 end
 
-CLONE_ACTIVE=false
+CLONE_ACTIVE=nil
 function INPUT_ABILITY()
 if (__SPECIAL_SWORD_THROWING==true) then
      FX_TIMESKIP()
@@ -1004,10 +1140,72 @@ if (__SPECIAL_SWORD_THROWING==true) then
           [1]={Damage=10.0,DamageType='',HitStop=5,HitStun=0, Inflict={'STOPshort'}, Knockback=GET_LOOK()*-25,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT3,Pitch=0.9,Volume=0.8}, Rehit=0, Host=Model_Body[HAND_R],Size=V3(3,6,4),Off=COORD(0,0,-8),Frames=6,HitOwner=false,Color=HB_COLOR_SWEET, Special={}}} }, FrameSpecial={[6]=function() SWORD_TRAIL=false IMMUNE=false end} }
      BUSY-=1
      ANIM_BUSY=false
+     return
      end
-if (BUSY<=0) then
+
+if (not CLONE_ACTIVE) then
+     -- ABILITY_A10 --
+     if (INPUTBEFORE=='↑') then
+     BUSY+=1
      SET_LOOK_AT_NEAREST()
-     FRAME{Frames=4}
+     local thisCLONE, thisCLONE_ANIM = CLONE { Time=40/60, Animation=ANIM_GET('ATTACK_A11') }
+     local valx = 4.2
+     local ticker = valx
+     SET_SOUND(SND_MAGIC3,Model_Body[ROOT],3.2,0.5)
+     SET_SOUND(SND_SPCSHORT,Model_Body[ROOT],1,0.6)
+     SET_SOUND(SND_CLOCK_BELL,Model_Body[ROOT],2,0.7)
+     FRAME{Frames=14, Pass=function(i) if (i%4==0) then thisCLONE:PivotTo(thisCLONE:GetPivot()*COORD(0,0,-ticker)) ticker=valx*1-((i/14)^0.5) AFTERIMAGE( thisCLONE ) end end}
+     BUSY-=1
+     SET_SOUND(SND_LUNGE,thisCLONE[HAND_R],0.7)
+     ATTACK { Frames=13, Whiff=0, Hitboxes={ [0]={
+          [0]={Damage=5.0,DamageType='',HitStop=5,HitStopSelf=-5,HitStun=0, Inflict={}, Knockback=GET_LOOK(thisCLONE)*20,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT3,Pitch=0.9,Volume=0.6}, Rehit=0, Host=thisCLONE[HAND_R],Size=V3(3,6,7),Off=COORD(0,0,-3.5),Frames=9,HitOwner=false,Color=HB_COLOR_SOUR, Special={}},
+          [1]={Damage=5.0,DamageType='',HitStop=5,HitStopSelf=-5,HitStun=0, Inflict={}, Knockback=GET_LOOK(thisCLONE)*25,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_CUT3,Pitch=0.9,Volume=0.8}, Rehit=0, Host=thisCLONE[HAND_R],Size=V3(3,6,4),Off=COORD(0,0,-8),Frames=9,HitOwner=false,Color=HB_COLOR_SWEET, Special={}}} }, FrameSpecial={} }
+     CLONE_ACTIVE=nil
+     return
+     end
+     
+     -- ABILITY_A12 --
+     if (INPUTBEFORE=='↓↑' and BUSY<=0) then
+     BUSY+=1
+     SET_LOOK_AT_NEAREST()
+     ANIM_QUERY(ANIM_GET('ABILITY_A12'))
+     KNOCKBACK { Model, Velocity=GET_LOOK()*1, Time=1 }
+     SET_SOUND(SND_SPCSHORT,Model_Body[ROOT],1,0.6)
+     FRAME{Frames=7, Pass=function(i) if (i%4==0) then AFTERIMAGE( Model ) end end}
+     KNOCKBACK { Model, Velocity=GET_LOOK()*-14, Time=0.1 }
+     SET_SOUND(SND_MAGIC3,Model_Body[ROOT],2.9,0.5)
+     SET_SOUND(SND_EPITAPH,Model_Body[ROOT],0.9,0.6)
+     SET_SOUND(SND_CLOCK_BELL,Model_Body[ROOT],2,0.7)
+
+     local thisCLONE, thisCLONE_ANIM = CLONE { Time=9/60, Animation=ANIM_GET('CLONE_DASH') }
+     local function CA () AFTERIMAGE( thisCLONE ) end
+     Service'TweenService':Create(thisCLONE.PrimaryPart,TweenInfo.new(8/60,Enum.EasingStyle.Linear,Enum.EasingDirection.In,0,false,0),{CFrame=thisCLONE:GetPivot()*COORD(0,0,-20)}):Play()
+     
+     ATTACK { Frames=11, Whiff=0, Hitboxes={ [0]={
+          [0]={Damage=6.5,DamageType='',HitStop=5,HitStopSelf=-5,HitStun=0, Inflict={'WARP'}, Knockback=GET_LOOK(thisCLONE)*45,KnockbackMax=nil,KnockbackTime=0.1, Sound={data=SND_GLITCH,Pitch=1.0,Volume=0.8}, Rehit=0, Host=thisCLONE[ROOT],Size=V3(3,6,7),Off=COORD(0,0,-3.5),Frames=8,HitOwner=false,Color=HB_COLOR_NORMAL, Special={}} } }, FrameSpecial={[2]=CA,[4]=CA,[6]=CA,[8]=CA}}
+     BUSY-=1
+     CLONE_ACTIVE=nil
+     return
+     end
+end
+
+if (BUSY<=0) then
+     if (not COOLDOWNS ['ABILITY']) then
+     SET_SOUND(SND_CLOCK_BELL,Model_Body[ROOT],4,0.5)
+     SET_SOUND(SND_MAGIC2,Model_Body[ROOT],2.5,0.7)
+     AFTERIMAGE ( Model )
+     CLOCK { Radius=12, Color=COLOR(1,1,1), Minute=15.0, Hour=3.0, Duration=1.0 }
+     local RANGE = 25
+     local RESULT = RAY ( Model:GetPivot().p, GET_LOOK()*RANGE )
+     local X,Y,Z = Model:GetPivot():ToEulerAnglesYXZ()
+     if (RESULT) then
+          Model:PivotTo(COORD(RESULT.Position)*ANGLE(X,Y,Z))
+     else
+          Model:PivotTo(Model:GetPivot()*COORD(0,0,-RANGE))
+     end
+     CLOCK { Radius=12, Color=COLOR(1,1,1), Minute=15.0, Hour=3.0, Duration=1.0 }
+     COOLDOWNS ['ABILITY'] = 60
+     end
 end
 end
 
